27dc759ac7fb561f1e4354016e261de6cda6d800
anandkelkar <kelkaranand28@gmail.com>
2018-02-15 15:09:11 -0500

    Anand - Added PageViewController

---
+++ CheckIn/CheckIn/SettingsController.swift
@@ -0,0 +1,15 @@
+//
+//  SettingsController.swift
+//  CheckIn
+//
+//  Created by Anand Kelkar on 15/02/18.
+//  Copyright © 2018 Sam Walk. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SettingsController :UIViewController
+{
+    
+}
