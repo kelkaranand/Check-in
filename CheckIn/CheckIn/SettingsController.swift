b9e6edca07eaa2de7ef859328fc93e212def8694
anandkelkar <kelkaranand28@gmail.com>
2018-02-27 14:04:38 -0500

    Anand - Push button alert

--- CheckIn/CheckIn/SettingsController.swift
+++ CheckIn/CheckIn/SettingsController.swift
@@ -17,33 +17,13 @@ class SettingsController :UIViewController
     
     //Action on Push Data button
     @IBAction func push(_ sender: UIButton) {
-        var databaseRef = Database.database().reference(withPath: "CheckIn")
         
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
-            return
-        }
-        let managedContext = appDelegate.persistentContainer.viewContext
-        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "CheckedInStudent")
-        
-        do {
-            var localData = try managedContext.fetch(fetchRequest)
-            
-            for data in localData
-            {
-                let post : [String: AnyObject] = ["id" : data.value(forKey: "id") as AnyObject, "media" : data.value(forKey: "media") as AnyObject, "guests" : data.value(forKey: "guests") as AnyObject]
-                let locationRef = databaseRef.childByAutoId()
-                locationRef.setValue(post)
-            }
-            
-            var alert = UIAlertController(title: "Data Uploaded", message: "Check-in data successfully uploaded to Firebase", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            self.present(alert, animated: true)
-            
-        } catch let error as NSError {
-            print ("Could not fetch data")
-        }
-        
-        
+        var alert=UIAlertController(title: "Data Upload", message: "You are about to upload the check-in data on this device into the database. Are you sure you want to continue?", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: {(alertAction : UIAlertAction) in
+            self.pushData()
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        self.present(alert, animated: true)
     }
     
     var students: [NSManagedObject] = []
@@ -70,6 +50,36 @@ class SettingsController :UIViewController
         let storageRef = Storage.storage().reference()
         
         
+    }
+    
+    //Function to push CheckIn data to Firebase
+    func pushData()
+    {
+        var databaseRef = Database.database().reference(withPath: "CheckIn")
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "CheckedInStudent")
+        
+        do {
+            var localData = try managedContext.fetch(fetchRequest)
+            
+            for data in localData
+            {
+                let post : [String: AnyObject] = ["id" : data.value(forKey: "id") as AnyObject, "media" : data.value(forKey: "media") as AnyObject, "guests" : data.value(forKey: "guests") as AnyObject]
+                let locationRef = databaseRef.childByAutoId()
+                locationRef.setValue(post)
+            }
+            
+            var alert = UIAlertController(title: "Data Uploaded", message: "Check-in data successfully uploaded.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true)
+            
+        } catch let error as NSError {
+            print ("Could not fetch data")
+        }
     }
     
 }
